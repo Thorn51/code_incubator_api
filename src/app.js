d2d2538b433afe56fc3a76a75c08cab9c3459707
Matt Thornberry <mtberry16@hotmail.com>
2020-01-27 16:43:46 -0700

    Add bearer validation

--- src/app.js
+++ src/app.js
@@ -13,6 +13,17 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  console.log(authToken, apiToken, process.env.API_TOKEN);
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
