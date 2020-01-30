8bafd159f3904269b4df76f749d7e82c15c6ec13
Matt Thornberry <mtberry16@hotmail.com>
2020-01-30 08:08:08 -0700

    Add function to create ideas test data

---
+++ test/fixtures.js
@@ -0,0 +1,36 @@
+function makeIdeasArray() {
+  return [
+    {
+      id: 1,
+      project_title: "First Project",
+      project_summary:
+        "Lorem ipsum dolor sit amet, te autem soluta facilisi vel, feugiat perfecto sapientem sit ei, in sit electram abhorreant. Ne nam aeterno labitur admodum, qui timeam quaerendum ullamcorper ut. Porro debet molestie eu duo, sea no essent feugait. In nec atqui scaevola, ea sed everti sanctus convenire. Ea quod discere pri, hinc incorrupte ne his.",
+      date_submitted: "2019-12-21T07:00:00.000Z",
+      status: "Idea",
+      github: "fake_url",
+      votes: "5"
+    },
+    {
+      id: 2,
+      project_title: "Second Project",
+      project_summary:
+        "Lorem ipsum dolor sit amet, te autem soluta facilisi vel, feugiat perfecto sapientem sit ei, in sit electram abhorreant. Ne nam aeterno labitur admodum, qui timeam quaerendum ullamcorper ut. Porro debet molestie eu duo, sea no essent feugait. In nec atqui scaevola, ea sed everti sanctus convenire. Ea quod discere pri, hinc incorrupte ne his.",
+      date_submitted: "2019-12-21T07:00:00.000Z",
+      status: "Idea",
+      github: "fake_url",
+      votes: "50"
+    },
+    {
+      id: 3,
+      project_title: "Third Project",
+      project_summary:
+        "Lorem ipsum dolor sit amet, te autem soluta facilisi vel, feugiat perfecto sapientem sit ei, in sit electram abhorreant. Ne nam aeterno labitur admodum, qui timeam quaerendum ullamcorper ut. Porro debet molestie eu duo, sea no essent feugait. In nec atqui scaevola, ea sed everti sanctus convenire. Ea quod discere pri, hinc incorrupte ne his.",
+      date_submitted: "2019-12-21T07:00:00.000Z",
+      status: "Idea",
+      github: "fake_url",
+      votes: "25"
+    }
+  ];
+}
+
+module.exports = { makeIdeasArray };
