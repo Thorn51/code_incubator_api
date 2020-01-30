b8c53f8f42a59d587439307a1c913c721d8f2752
Matt Thornberry <mtberry16@hotmail.com>
2020-01-30 08:07:28 -0700

    Add methods to the ideas service object

--- src/ideas/ideas-service.js
+++ src/ideas/ideas-service.js
@@ -1,6 +1,32 @@
 const IdeasService = {
   getAllIdeas(knex) {
     return knex.select("*").from("ideas");
+  },
+  insertIdea(knex, newIdea) {
+    return knex
+      .insert(newIdea)
+      .into("ideas")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex
+      .from("ideas")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  deleteIdea(knex, id) {
+    return knex("ideas")
+      .where({ id })
+      .delete();
+  },
+  updateIdea(knex, id, updateFields) {
+    return knex("ideas")
+      .where({ id })
+      .update(updateFields);
   }
 };
 
