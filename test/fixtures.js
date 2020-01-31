65ce387116f2768836a878de2e9b30c5bf7cf68e
Matt Thornberry <mtberry16@hotmail.com>
2020-01-31 13:57:27 -0700

    Add author, comments array and users array

--- test/fixtures.js
+++ test/fixtures.js
@@ -8,7 +8,8 @@ function makeIdeasArray() {
       date_submitted: "2019-12-21T07:00:00.000Z",
       status: "Idea",
       github: "fake_url",
-      votes: "5"
+      votes: "5",
+      author: 1
     },
     {
       id: 2,
@@ -18,7 +19,8 @@ function makeIdeasArray() {
       date_submitted: "2019-12-21T07:00:00.000Z",
       status: "Idea",
       github: "fake_url",
-      votes: "50"
+      votes: "50",
+      author: 2
     },
     {
       id: 3,
@@ -28,7 +30,8 @@ function makeIdeasArray() {
       date_submitted: "2019-12-21T07:00:00.000Z",
       status: "Idea",
       github: "fake_url",
-      votes: "25"
+      votes: "25",
+      author: 3
     }
   ];
 }
@@ -43,9 +46,164 @@ function makeXssIdea() {
       date_submitted: "2020-01-30T07:00:00.000Z",
       status: "Idea",
       github: "",
-      votes: 0
+      votes: 0,
+      author: 2
     }
   ];
 }
 
-module.exports = { makeIdeasArray, makeXssIdea };
+function makeComments() {
+  return [
+    {
+      id: "1",
+      user_id: "2",
+      project_id: "1",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "15"
+    },
+    {
+      id: "2",
+      user_id: "3",
+      project_id: "1",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "9"
+    },
+    {
+      id: "3",
+      user_id: "4",
+      project_id: "1",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "25"
+    },
+    {
+      id: "4",
+      user_id: "1",
+      project_id: "2",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "0"
+    },
+    {
+      id: "5",
+      user_id: "3",
+      project_id: "2",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "7"
+    },
+    {
+      id: "6",
+      user_id: "4",
+      project_id: "2",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "10"
+    },
+    {
+      id: "7",
+      user_id: "1",
+      project_id: "3",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "3"
+    },
+    {
+      id: "8",
+      user_id: "2",
+      project_id: "3",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "6"
+    },
+    {
+      id: "9",
+      user_id: "4",
+      project_id: "3",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "12"
+    },
+    {
+      id: "10",
+      user_id: "1",
+      project_id: "4",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "1"
+    },
+    {
+      id: "11",
+      user_id: "2",
+      project_id: "4",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "16"
+    },
+    {
+      id: "12",
+      user_id: "3",
+      project_id: "4",
+      comment_text:
+        "Ne velit facilis deserunt nam, eum lucilius constituto ex. Odio rebum dignissim ad mel, an ius tollit veniam, vix vidit mazim homero ad. Paulo definitiones et mei, nam ne illum scripta. Zril senserit at has, at est clita eirmod moderatius. Et mnesarchum posidonium sea, aliquid debitis oportere qui et. Vix no ullum adipiscing, an qui nulla temporibus.",
+      date_submitted: "12.21.20",
+      votes: "3"
+    }
+  ];
+}
+
+function makeUsersArray() {
+  return [
+    {
+      id: "1",
+      first_name: "John",
+      last_name: "Doe",
+      email: "jdoe@devevlopmenttesting.com",
+      password: "notRealForDev!1",
+      nickname: "code_guru",
+      votes: "77"
+    },
+    {
+      id: "2",
+      first_name: "John",
+      last_name: "Doe",
+      email: "jdoe@devevlopmenttesting.com",
+      password: "notRealForDev1!",
+      nickname: "react_champ",
+      votes: "35"
+    },
+    {
+      id: "3",
+      first_name: "John",
+      last_name: "Doe",
+      email: "jdoe@devevlopmenttesting.com",
+      password: "notRealForDev1!",
+      nickname: "jquery_king",
+      votes: "20"
+    },
+    {
+      id: "4",
+      first_name: "John",
+      last_name: "Doe",
+      email: "jdoe@devevlopmenttesting.com",
+      password: "notRealForDev1!",
+      nickname: "css_aficianado",
+      votes: "15"
+    }
+  ];
+}
+
+module.exports = { makeIdeasArray, makeXssIdea, makeComments };
