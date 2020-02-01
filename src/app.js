2b848299cb7cbda9ad316273e1354140955bc167
Matt Thornberry <mtberry16@hotmail.com>
2020-02-01 07:47:02 -0700

    Prefix api to users route

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/ideas", ideasRouter);
 app.use("/comments", commentsRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
