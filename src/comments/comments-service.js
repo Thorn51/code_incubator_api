13453394b66e3a1ac16fe6966c971b34c2d4d72a
Matt Thornberry <mtberry16@hotmail.com>
2020-02-02 03:09:16 +0000

    Setup methods

---
+++ src/comments/comments-service.js
@@ -0,0 +1,33 @@
+const CommentsService = {
+  getAllComments(knex) {
+    return knex.select("*").from("comments");
+  },
+  insertComment(knex, newComment) {
+    return knex
+      .insert(newComment)
+      .into("comments")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex
+      .from("comments")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  deleteComment(knex, id) {
+    return knex("comments")
+      .where({ id })
+      .delete();
+  },
+  updateComment(knex, id, updateFields) {
+    return knex("comments")
+      .where({ id })
+      .update(updateFields);
+  }
+};
+
+module.exports = CommentsService;
