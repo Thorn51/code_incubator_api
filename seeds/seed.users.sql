52849771065eef81f60609574bd7f3a233b91ca8
Matt Thornberry <mtberry16@hotmail.com>
2020-02-03 07:48:08 -0700

    Seed users into db

---
+++ seeds/seed.users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password, nickname, votes, date_created) 
+    VALUES('John', 'Doe', 'jdoe@developmenttesting.com', 'notRealForDevl!1', 'code_guru', '77', '2020-12-21T07:00:00.000Z'),
+    ('Luke', 'Doe', 'ldoe@developmenttesting.com', 'notRealForDevl!1', 'react_champ', '35', '2020-12-21T07:00:00.000Z'),
+    ('Bob', 'Doe', 'bdoe@developmenttesting.com', 'notRealForDevl!1', 'jquery_king', '20', '2020-12-21T07:00:00.000Z'),
+    ('Steve', 'Doe', 'sdoe@developmenttesting.com', 'notRealForDevl!1', 'css_aficianado', '15', '2020-12-21T07:00:00.000Z');
