b86d49bb30f24b79edc33694cb7d5a66c0c80b10
Matt Thornberry <mtberry16@hotmail.com>
2020-02-06 07:33:25 -0700

    Import logger

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,5 @@
+const logger = require("../logger");
+
 function requireAuth(req, res, next) {
   const authToken = req.get("Authorization") || "";
   let basicToken;
@@ -32,7 +34,6 @@ function requireAuth(req, res, next) {
 function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-  console.log(authToken);
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: "Unauthorized request" });
