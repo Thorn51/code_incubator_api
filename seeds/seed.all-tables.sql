4488b3692b5ec8a706f075a8a0744d0edafe032d
Matt Thornberry <mtberry16@hotmail.com>
2020-02-06 20:53:54 +0000

    Apply bcrypt to passwords

--- seeds/seed.all-tables.sql
+++ seeds/seed.all-tables.sql
@@ -1,10 +1,10 @@
 TRUNCATE comments, ideas, users RESTART IDENTITY CASCADE;
 
 INSERT INTO users (first_name, last_name, email, password, nickname, votes, date_created) 
-    VALUES('John', 'Doe', 'jdoe@developmenttesting.com', 'notRealForDevl!1', 'code_guru', '77', '2020-12-21T07:00:00.000Z'),
-    ('Luke', 'Doe', 'ldoe@developmenttesting.com', 'notRealForDevl!1', 'react_champ', '35', '2020-12-21T07:00:00.000Z'),
-    ('Bob', 'Doe', 'bdoe@developmenttesting.com', 'notRealForDevl!1', 'jquery_king', '20', '2020-12-21T07:00:00.000Z'),
-    ('Steve', 'Doe', 'sdoe@developmenttesting.com', 'notRealForDevl!1', 'css_aficianado', '15', '2020-12-21T07:00:00.000Z');
+    VALUES('John', 'Doe', 'jdoe@developmenttesting.com', '$2a$12$d515ftTi6hRTdV.0r1xr5.Hbrd8V3W0yIA0guPghw6I6DQRADzCGm', 'code_guru', '77', '2020-12-21T07:00:00.000Z'),
+    ('Luke', 'Doe', 'ldoe@developmenttesting.com', '$2a$12$S6MdndQF3HX6H8bLzMbl0ekpTvjiEb4rTal92xFuBcxY4MNfnERxC', 'react_champ', '35', '2020-12-21T07:00:00.000Z'),
+    ('Bob', 'Doe', 'bdoe@developmenttesting.com', '$2a$12$X6XpGevXZ6pFgmp4PTq94OyKl17eSGY2gDcheocYcwCFKiLHR8DGa', 'jquery_king', '20', '2020-12-21T07:00:00.000Z'),
+    ('Steve', 'Doe', 'sdoe@developmenttesting.com', '$2a$12$NeuK9BNnjouafYcSXfshd.QRSErVD4K.S4N0LPhjQzC1/ymCJf46m', 'css_aficianado', '15', '2020-12-21T07:00:00.000Z');
 
 INSERT INTO ideas (project_title, project_summary, date_submitted, github, votes, status, author)
 VALUES
