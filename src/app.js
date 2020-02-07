1bbc736c750da56b219cf9ad3092dc2e858cf660
Matt Thornberry <mtberry16@hotmail.com>
2020-02-07 11:08:40 -0700

    Add auth router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { NODE_ENV } = require("./config");
 const ideasRouter = require("./ideas/ideas-router");
 const commentsRouter = require("./comments/comments-router");
 const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cors());
 app.use("/api/ideas", ideasRouter);
 app.use("/api/comments", commentsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/auth", authRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
