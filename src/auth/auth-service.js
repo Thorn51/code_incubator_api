a6d19d1f72a1709d7542da06c27d5ca0aa8c1bcc
Matt Thornberry <mtberry16@hotmail.com>
2020-02-09 14:10:08 -0700

    Add methods  for JWT and password comparison

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,8 +1,27 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
 const AuthService = {
   getUserWithUserName(db, email) {
-    return db("users")
+    return db
+      .from("users")
       .where({ email })
       .first();
+  },
+  comparePasswords(password, hash) {
+    return bcrypt.compare(password, hash);
+  },
+  createJwt(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {
+      subject,
+      algorithm: "HS256"
+    });
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ["HS256"]
+    });
   },
   parseBasicToken(token) {
     return Buffer.from(token, "base64")
