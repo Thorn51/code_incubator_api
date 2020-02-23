032f67634b45ee6d43bd9b73cd5865aeaa6df3a1
Matt Thornberry <mtberry16@hotmail.com>
2020-02-22 18:53:21 -0700

    Create methods to alter comment_vote table

---
+++ src/comment-vote/comment-vote-service.js
@@ -0,0 +1,34 @@
+//Perform operations on the comments_votes table in db
+const CommentVoteService = {
+  getAllCommentVotes(knex) {
+    return knex.select("*").from("comment_vote");
+  },
+  insertVote(knex, vote) {
+    return knex
+      .insert(vote)
+      .into("comment_vote")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex
+      .from("comment_vote")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  deleteVote(knex, id) {
+    return knex("comment_vote")
+      .where({ id })
+      .delete();
+  },
+  updateVote(knex, id, updateVote) {
+    return knex("comment_vote")
+      .where({ id })
+      .update(updateVote);
+  }
+};
+
+module.exports = CommentVoteService;
